6b7287caf1617e3c6e95a7d18353c9af65620d81
Carl di Ortus <reklamukibiras@gmail.com>
2014-03-22 16:53:09 +0200

    add arg-parser

--- main.py
+++ main.py
@@ -6,16 +6,29 @@
 # Available in MIT license (see LICENCE)
 
 from wavegen import *
+import argparse
+
+parser = argparse.ArgumentParser(description="""
+    Generates a wave file for given frequency and type.
+    Default frequency is 440 Hz (Concert A).
+    Currently only sine wave type is supported.""")
+parser.add_argument('filename', metavar='<filename>', type=str,
+                   help='location to save the generated wave file')
+parser.add_argument('-f', metavar='freq' , type=float, default=440.0,
+                   help='frequency of a wave sample')
 
 
-class Main:
-    
-    def generateWave(filename, type=0, freq=440.0):
+args = parser.parse_args()
+
+
+def generateWave(filename, type=0, freq=440.0):
         """Generates a wave and saves it to a file"""
         
         wave = wavegen()
         wave.wavegen(type, freq)
         wave.save(filename);
-        # SoundPlayer player = new SoundPlayer(filePath);               
-        # player.Play();
-    
+        # TODO: player.Play(filename)
+
+
+# TODO: extract type to argparser, when more types added
+generateWave(args.filename, type=0, freq=args.f)
